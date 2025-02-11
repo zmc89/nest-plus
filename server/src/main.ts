7fd8dbd5ff28cc07722659d8c5834216bdc93920
zengmc <9142215@qq.com>
2025-02-11 17:28:45 +0800

    feat:安装prisma

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,11 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule,{
+    cors: true, // 开启跨域访问
+  })
   /** 全局验证 */
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
-  await app.listen(process.env.PORT ?? 3000);
+  /** 配置项 */
+  const config = app.get(ConfigService);
+  /** 端口 */
+  const port = config.get<number>('app.port') || 8080;
+  await app.listen(port);
 }
 bootstrap();
